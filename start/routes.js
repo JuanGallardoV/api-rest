b6c59b3c891a2df7c739ab0430392dc09aa398dd
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-23 13:11:33 -0300

    Creacion de usuarios

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,10 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+// Route.get('/', () => {
+//   return { greeting: 'Hello world in JSON' }
+// })
+
+Route.group(() => {
+  Route.post('users/register', 'UserController.store')
+}).prefix('api/v1/')
