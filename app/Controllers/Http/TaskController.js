589d07a7b548d7032577c35922c76cd2090d931b
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-23 15:29:02 -0300

    Creado el metodo create para Tasks

---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Project = use('App/Models/Project')
+const Task = use('App/Models/Task')
+const AuthService = use('App/Services/AuthService')
+
+class TaskController {
+    async create({ auth, request, params }) {
+        const user = await auth.getUser()
+        const { description } = request.all()
+        const { id } = params
+        const project = await Project.find(id)
+        AuthService.checkPermission(project, user)
+        const task = new Task()
+        task.fill({
+            description
+        })
+        await project.tasks().save(task)
+        return task
+    }
+}
+
+module.exports = TaskController
